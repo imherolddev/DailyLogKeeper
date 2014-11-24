c9151bf9b8ec787dd5f0f36c0ac5bc6cba6c1f90
imherolddev <imherolddev@gmail.com>
2014-11-23 16:16:48 -0800

    Photos
    -adding photo capability

--- app/src/main/java/com/imherolddev/dailylogkeeper/activities/NewLogActivity.java
+++ app/src/main/java/com/imherolddev/dailylogkeeper/activities/NewLogActivity.java
@@ -3,7 +3,10 @@ package com.imherolddev.dailylogkeeper.activities;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,6 +20,9 @@ import android.widget.EditText;
 import com.imherolddev.dailylogkeeper.R;
 import com.imherolddev.dailylogkeeper.models.DailyLog;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class NewLogActivity extends ActionBarActivity {
@@ -36,6 +42,8 @@ public class NewLogActivity extends ActionBarActivity {
     private boolean edited = false;
 
     private int uid;
+
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -128,9 +136,6 @@ public class NewLogActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
 
-            case R.id.action_settings:
-                return true;
-
             case R.id.action_save:
 
                 Date date;
@@ -142,13 +147,13 @@ public class NewLogActivity extends ActionBarActivity {
                 DailyLog log = new DailyLog(uid, et_jobName.getText()
                         .toString(), et_logTitle.getText().toString(), et_logEntry
                         .getText().toString(), date);
+                log.setPhotoUri(currentPhotoPath);
 
                 returnIntent = new Intent();
                 returnIntent.putExtra("log", log);
                 setResult(RESULT_OK, returnIntent);
 
                 finish();
-
                 return true;
 
             case R.id.action_discard:
@@ -177,6 +182,12 @@ public class NewLogActivity extends ActionBarActivity {
                         }).create().show();
 
                 setResult(RESULT_CANCELED, returnIntent);
+                return true;
+
+            case R.id.action_photo:
+
+                showPhotoDialog();
+                return true;
 
             default:
                 return super.onOptionsItemSelected(item);
@@ -192,6 +203,100 @@ public class NewLogActivity extends ActionBarActivity {
         setResult(RESULT_CANCELED, returnIntent);
 
         super.onBackPressed();
+
+    }
+
+    private void showPhotoDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle(R.string.action_photo_title)
+                .setItems(R.array.choose_add_photo, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        switch (which) {
+
+                            case 0:
+
+                                //launch photo gallery, get URI of selected
+
+
+                                break;
+
+                            case 1:
+
+                                //launch camera, get URI of photo
+                                dispatchTakePicture();
+
+                                break;
+
+                        }
+
+                    }
+                });
+
+        builder.create().show();
+
+    }
+
+    private File createImageFile() throws IOException {
+
+        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "JPEG_" + time + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                fileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+
+    }
+
+    private void dispatchTakePicture() {
+
+        Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Ensure there is a camera available
+        if (photoIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ioex) {
+                //Error occurred
+            }
+
+            if (photoFile != null) {
+
+                photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(photoIntent, 1);
+
+            }
+
+        }
+
+    }
+
+    private void galleryAddPic() {
+
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File file = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(file);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            galleryAddPic();
+        }
 
     }
 
